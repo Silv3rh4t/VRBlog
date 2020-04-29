e001e824f15899543788a7fdb1716e096f2c40cf
Akhand Yaduvanshi <Silv3rh4t@users.noreply.github.com>
2020-04-29 13:18:32 +0530

    Initial commit

---
+++ js/login.js
@@ -0,0 +1,13 @@
+function signUpShow()
+{
+  document.getElementById("signup").disabled= true;
+  document.getElementById("signin").disabled= false;
+  document.getElementById("in-form").hidden= true;
+  document.getElementById("up-form").hidden= false;
+}
+function logInShow() {
+  document.getElementById("signup").disabled= false;
+  document.getElementById("signin").disabled= true;
+  document.getElementById("in-form").hidden= false;
+  document.getElementById("up-form").hidden= true;
+}

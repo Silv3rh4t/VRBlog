fd4397abbc3aa5c795bc670a963701d11ae29945
Akhand Yaduvanshi <Silv3rh4t@users.noreply.github.com>
2020-05-02 21:37:25 +0530

    Login page frontend complete

--- js/login.js
+++ js/login.js
@@ -3,13 +3,11 @@ function signUpShow()
   document.getElementById("signup").disabled= true;
   document.getElementById("signin").disabled= false;
   document.getElementById("in-form").hidden= true;
-  document.getElementById("in-oauth").hidden= true;
   document.getElementById("up-form").hidden= false;
 }
 function logInShow() {
   document.getElementById("signup").disabled= false;
   document.getElementById("signin").disabled= true;
   document.getElementById("in-form").hidden= false;
-  document.getElementById("in-oauth").hidden= false;
   document.getElementById("up-form").hidden= true;
 }
